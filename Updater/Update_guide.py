3dc3f0db9372d2699a468f3e0b63f18bd8ada6b3
mohamed chafik <mo.chafik26@gmail.com>
2026-08-30 15:17:31 +0100

    sending parameters to change json

--- Updater/Update_guide.py
+++ Updater/Update_guide.py
@@ -1,10 +1,9 @@
 import json
 import os
-def update():
-    with open("../Backend/Guides/Rust.json", "r") as file:
-        guide = json.load(file)
-    guide["install"]["arch"] = ["test"]
+def update(id, install, verify):
+    print(id, install)
+    #with open(`../Backend/Guides/${id}.json`, "r") as file:
+     #   guide = json.load(file)
 
-    with open("../Backend/Guides/Rust.json", "w") as file:
-        json.dump(guide, file, indent = 4)
-update()
+    #with open(`../Backend/Guides/${id}.json`, "w") as file:
+     #   json.dump(guide, file, indent = 4)
--- Updater/main.py
+++ Updater/main.py
@@ -1,9 +1,9 @@
 from LoopTech import loop
 from Request import generate_guide
+from Update_guide import update
 def main():
     values = loop()
     for i in values:
-        guide = generate_guide("rust")
-        print(guide)
+        guide = generate_guide(i)
+        update(guide.name, guide.install, guide.verify)
 main()
-
