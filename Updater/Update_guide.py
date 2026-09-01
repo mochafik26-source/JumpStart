9619ea01ab750e49bb232c506dbbea5eac057b33
mohamed chafik <mo.chafik26@gmail.com>
2026-09-01 14:40:59 +0100

    convertin ai ooutput to json

--- Updater/Update_guide.py
+++ Updater/Update_guide.py
@@ -1,8 +1,9 @@
 import json
 import os
-def update(id: str, install: list[str]):
+def update(id: str, install: str):
     with open(f"../Backend/Guides/{id}.json", "r") as file:
         guide = json.load(file)
+    install = json.load(install)
     guide["install"] = install["install"]
     with open(f"../Backend/Guides/{id}.json", "w") as file:
         json.dump(guide, file, indent = 4)
