c3b3f41cca6e959a457bf6bac00080a852008205
mohamed chafik <mo.chafik26@gmail.com>
2026-09-02 14:39:25 +0100

    script to update the guides

--- Updater/Update_guide.py
+++ Updater/Update_guide.py
@@ -4,10 +4,7 @@ def update(id: str, installer: str):
     print(id)
     with open(f"../Backend/Guides/{id}.json", "r") as file:
         guide = json.load(file)
-    #guide = gd
     instal = json.loads(installer)
-    #guide["install"] = instal["install"]
-    print(guide)
-    #with open(f"../Backend/Guides/{id}.json", "w") as file:
-     #   json.dump(guide, file, indent = 4)
-    #print("done editing") 
+    guide = instal
+    with open(f"../Backend/Guides/{id}.json", "w") as file:
+        json.dump(guide, file, indent = 4)
